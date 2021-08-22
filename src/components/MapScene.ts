2c976b1f95aaa3b43ec93162a0e3f1f58a81ea17
Calvin McMurray <calvin.r.mcm@gamil.com>
2021-08-22 17:32:55 -0500

    Adds some new sprites.

--- src/components/MapScene.ts
+++ src/components/MapScene.ts
@@ -8,7 +8,10 @@ export class MapScene extends Phaser.Scene {
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     private storeUnsubscribe: Unsubscribe | null = null;
     private players: {name: string, controller: string, sprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody }[] = [
-        {name: "p1", controller: "keyboard", sprite: null as any}
+        {name: "p1", controller: "keyboard", sprite: null as any},
+        {name: "p2", controller: "keyboard", sprite: null as any},
+        {name: "p3", controller: "keyboard", sprite: null as any},
+        {name: "p4", controller: "keyboard", sprite: null as any}
     ];
     private me: number;
 
@@ -21,6 +24,8 @@ export class MapScene extends Phaser.Scene {
 
     preload() {
         this.load.spritesheet("fem", "/public/f0202.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet("mal", "/public/m0501.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet("sol", "/public/s0402.png", { frameWidth: 32, frameHeight: 32 });
         this.load.image("LPC_Remodeled", "/public/LPC_Remodeled.png");
         this.load.image("bridge", "/public/bridge.png");
         this.load.image("Collisions", "/public/Collisions.png");
@@ -47,8 +52,18 @@ export class MapScene extends Phaser.Scene {
         this.anims.create({key: "walk-up", repeat: -1, frameRate: 12, yoyo: true, frames: this.anims.generateFrameNumbers("fem", {start:9, end:11})})
         // this.players[0].sprite.play("walk-down");
 
-        this.players.forEach(player => {
-            player.sprite = this.physics.add.sprite(160, 160, "fem", 1);
+        this.players.forEach((player, i) => {
+            switch(i){
+                case 0:
+                    player.sprite = this.physics.add.sprite(160, 160, "fem", 1);
+                    break;
+                case 1:
+                    player.sprite = this.physics.add.sprite(192, 160, "mal", 1);
+                    break;
+                default:
+                    player.sprite = this.physics.add.sprite(160 + i*32, 160, "sol", 1);
+                    break;
+            }
             player.sprite.body.setSize(24, 20);
             player.sprite.body.offset.x = 4;
             player.sprite.body.offset.y = 12;
@@ -70,8 +85,18 @@ export class MapScene extends Phaser.Scene {
             });
         });
 
-        this.cameras.main.setBounds(0,0, 64 * 32, 64 * 32);
         this.cameras.main.startFollow(this.players[this.me].sprite).setZoom(1.5);
+        this.cameras.main.setName("0")
+        this.cameras.add(window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight / 2, false, "1");
+        this.cameras.add(window.innerWidth / 2, window.innerHeight / 2, window.innerWidth / 2, window.innerHeight / 2, false, "2");
+        this.cameras.add(0, window.innerHeight / 2, window.innerWidth / 2, window.innerHeight / 2, false, "3");
+
+        this.players.forEach((player, i) => {
+            const cam = this.cameras.getCamera(i.toString());
+            cam.startFollow(player.sprite);
+            cam.setBounds(0,0, 64 * 32, 64 * 32);
+            cam.setSize(window.innerWidth / 2, window.innerHeight / 2);
+        })
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
